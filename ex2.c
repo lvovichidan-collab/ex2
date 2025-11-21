a08fd074c0a324489890a8643e692d55c7d2a764
lvovichidan-collab <lvovich.idan@gmail.com>
2025-11-21 16:46:21 +0200

    a12

---
+++ ex2.c
@@ -0,0 +1,28 @@
+/******************
+Name:
+ID:
+Assignment:
+*******************/
+
+#include <stdio.h>
+
+int main()
+{
+    // MAIN MENU LOOP
+
+    // TASK 1: Ducky's Unity Game
+
+    // TASK 2: The Memory Game
+
+    // TASK 3: Professor Pat's Power Calculation
+
+    // TASK 4: The Duck Parade
+
+    // TASK 5: The Mystery of the Repeated Digits
+
+    // TASK 6: EXIT
+
+    // dont forget through all the necessary parts to validate invalid input.
+
+    return 0;
+}
